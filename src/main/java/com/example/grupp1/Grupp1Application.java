1fa56f032191bc63fdccbc09ca65de7c80cf0d64
Moutasem-AH <moutasem.a.h@live.se>
2023-01-26 15:53:58 +0100

    wdd

--- src/main/java/com/example/grupp1/Grupp1Application.java
+++ src/main/java/com/example/grupp1/Grupp1Application.java
@@ -2,7 +2,7 @@ package com.example.grupp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//comment
 @SpringBootApplication
 public class Grupp1Application {
 
