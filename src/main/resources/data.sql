81be491cd025fba2443be9032302a1d17ecf57ee
LeopoldHolgersson <leopold.holgersson@gmail.com>
2023-01-31 16:29:52 +0100

    Bytt ut ikoner på level1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,26 +26,26 @@ INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('lekplats.png','Lekplats', 1)
 -- Answers question 2 lvl 1
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) VALUES ('parkbänk.png','Parkbänk', 2)
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('buske.png','Buske', 2)
-INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('plastkärl.png','Plastkärl', 2)
+INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('flaskåtervinning.png','Flaskåtervinning', 2)
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('lekplats.png','Lekplats', 2)
 
 -- Answers question 3 lvl 1
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) VALUES ('batterikärl.png','Batterikärl', 3)
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('lekplats.png','Lekplats', 3)
-INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('plastkärl.png','Plastkärl', 3)
-INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('skräpkärl.gif','Skräpkärl', 3)
+INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('papperskorg.png','Papperskorg', 3)
+INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('flaskåtervinning.png','Flaskåtervinning', 3)
 
 -- Answers question 4 lvl 1
-INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) VALUES ('batterikärl.png','batterikärl', 4)
+INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) VALUES ('glaskärl.png','Glaskärl', 4)
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('parkbänk.png','Parkbänk', 4)
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('metallkärl2.png','Metallkärl', 4)
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('lekplats.png','Lekplats', 4)
 
 -- Answers question 5 lvl 1
-INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) VALUES ('sopkorg.png','Sopkorg', 5)
+INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) VALUES ('papperskorg.png','Papperskorg', 5)
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('glaskärl.png','Glaskärl', 5)
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('buske.png','Buske', 5)
-INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('plastkärl.png','Plastkärl', 5)
+INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) values ('flaskåtervinning.png','Flaskåtervinning', 5)
 
 -- Answers question 1 lvl 2 TEST
 INSERT INTO ANSWER(PICTURE,ALT, PAGE_ID) VALUES ('cykel.png','Cykel', 6)
