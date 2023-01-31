14146df8efeeeb7b42dd1aa88b709d2352c45a20
Moutasem-AH <moutasem.a.h@live.se>
2023-01-31 10:35:03 +0100

    Audio

--- src/main/resources/static/action.js
+++ src/main/resources/static/action.js
@@ -1,6 +1,7 @@
 
 const coll = document.querySelector(".errormsg");
-
+var wrongAnswer = new Audio('audio/Felsvar.mp4');
+var rightAnswer = new Audio('audio/Rättsvar.mp4');
 
 function showId(correct, id){
 var error = document.getElementById("errorMsg")
@@ -9,15 +10,22 @@ if(correct == id){
 console.log("du har rätt")
 error.classList.add("hidden");
 correctMsg.classList.remove("hidden");
+   if (rightAnswer.paused || wrongAnswer.paused) {
+        rightAnswer.play();
+    }else{
+        wrongAnswer.currentTime= 0;
+        rightAnswer.currentTime = 0;
+    }
 }
 else{
 correctMsg.classList.add("hidden");
 error.classList.add("hidden")
 error.classList.remove("hidden");
 console.log("du har fel")
+   if (wrongAnswer.paused) {
+        wrongAnswer.play();
+    }else{
+        wrongAnswer.currentTime = 0;
+    }
 }
-
-//x.innerHtml= "Du har tryckt på fel"
-
-
 }
