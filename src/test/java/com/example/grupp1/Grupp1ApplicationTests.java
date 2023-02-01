5947c42919e82a91e52b57aa9305d58925ae4ca0
LeopoldHolgersson <leopold.holgersson@gmail.com>
2023-02-01 13:32:42 +0100

    Uppdaterad TODO efter lunch.

--- src/test/java/com/example/grupp1/Grupp1ApplicationTests.java
+++ src/test/java/com/example/grupp1/Grupp1ApplicationTests.java
@@ -1,20 +1,41 @@
 package com.example.grupp1;
 
+import com.example.grupp1.models.Answer;
+import com.example.grupp1.models.GameLevel;
+import com.example.grupp1.models.Page;
+import com.example.grupp1.repository.LevelRepository;
+import com.example.grupp1.repository.PageRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class Grupp1ApplicationTests {
+
+	@Autowired
+	private PageRepository pageRepository;
+	@Autowired
+	LevelRepository levelRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
-/*	@Test
-	void find() {
-		Room garage = new Room("Garage");
-		String result = garage.getRoom();
-		Assertions.assertEquals("Garage", result);
-	}*/
+	@Test
+	void findAnswerAltName() {
+		Answer test = new Answer(1L, "sandlåda.png", "Sandlåda");
+		String result = test.getAlt();
+		Assertions.assertEquals("Sandlåda", result);
+	}
+
+	@Test
+	void findAllGameLevel() {
+		List<GameLevel> list = (List)levelRepository.findById(1L).get();
+
+		Assertions.assertEquals("Sandlåda", list);
+	}
 }
